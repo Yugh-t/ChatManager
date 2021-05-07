d2118e35daa5c7a6f2c1db644bdcd1f06316a545
Yugh-t <yugh-t.drake@mail.ru>
2021-05-07 20:24:30 +0300

    Create a main plugin file

---
+++ src/ChatManager/Yugh/ChatManager.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace src\ChatManager\Yugh;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+use src\ChatManager\Yugh\listener\EventListener;
+
+class ChatManager extends PluginBase {
+  
+  public $config;
+  
+  public function onEnable() :void {
+    $this->config = new Config($this->getDataFolder(). "config.json", Config::JSON);
+    $this->getServer()->getPluginManager()->registerEvents(EventListener::create, $this);
+  }
+}

6dcfae06ac1d700fbe1ba53bd2df5b93e8b73364
Yugh-t <yugh-t.drake@mail.ru>
2021-05-07 20:39:56 +0300

    Update ChatManager.php

--- src/ChatManager/Yugh/ChatManager.php
+++ src/ChatManager/Yugh/ChatManager.php
@@ -9,11 +9,19 @@ use ChatManager\Yugh\listener\EventListener;
 
 class ChatManager extends PluginBase {
   
+  private const DISTANCE;
+  
   public $config;
   
   public function onEnable() :void {
     $this->config = new Config($this->getDataFolder(). "config.json", Config::JSON);
     $this->getServer()->getPluginManager()->registerEvents(EventListener::create, $this);
+    
+    $this->loadConfig();
+  }
+  
+  private function loadConfig() :void {
+    if (!$this->config->exists("distance")) $this->config->set("distance", self::DISTANCE);
   }
   
 }

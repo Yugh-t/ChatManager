f4d4e64282f41e41a144f3b07dd9d14aa1cc54e9
Yugh-t <yugh-t.drake@mail.ru>
2021-05-09 17:17:15 +0300

    Update EventListener.php

--- src/ChatManager/Yugh/listener/EventListener.php
+++ src/ChatManager/Yugh/listener/EventListener.php
@@ -3,30 +3,47 @@
 namespace ChatManager\Yugh\listener;
 
 use pocketmine\event\Listener;
+
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class EventListener implements Listener {
   
-  private $plugin;
+    private $plugin;
   
-  public function __construct($plugin) {
-    $this->plugin = $plugin;
-  }
-  
-  public function onChat(PlayerChatEvent $event) :void {
-    $player = $event->getPlayer();
-    $players = $player->getLevel()->getPlayers();
-    
-    $distance = $this->plugin->config->get("distance");
-    
-    foreach ($players as $ps) {
-      if ($ps->distance($player) <= $distance) {
-        $event->setCancelled(true);
-
-        $message = $event->getMessage();
-        $ps->sendMessage("[".$player->getName()."] ".$message);
-      }
+    public function __construct($plugin) {
+        $this->plugin = $plugin;
     }
-  }
+  
+    public function onChat(PlayerChatEvent $event) :void {
+        $player = $event->getPlayer();
+        $players = $player->getLevel()->getPlayers();
+        
+        $distance = $this->plugin->config->get("distance");
+    
+        foreach ($players as $ps) {
+            if ($ps->distance($player) <= $distance) {
+                $event->setCancelled(true);
+
+                $message = $event->getMessage();
+                $ps->sendMessage("[".$player->getName()."] ".$message);
+            }
+        }
+    }
+
+    public function onJoin(PlayerJoinEvent $event) :void {
+        $name = $event->getPlayer()->getName();
+
+        if ($this->plugin->config->exists(strtolower($name))) {
+            $config = $this->plugin->config->get(strtolower($name));
+            $messages = $this->plugin->messages->getMessage("join-message-command");
+
+            $format = $messages["send-format"];
+            $format = str_replace("{name}", $name, $format);
+            $format = str_replace("{message}", $config["join-message"], $format);
+
+            $this->plugin->getServer()->broadcastMessage($format);
+        }
+    }
   
 }

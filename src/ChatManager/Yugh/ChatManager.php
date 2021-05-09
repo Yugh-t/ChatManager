26cfe3aad090e23407b92d8f95187f3690876312
Yugh-t <yugh-t.drake@mail.ru>
2021-05-09 17:16:55 +0300

    Update ChatManager.php

--- src/ChatManager/Yugh/ChatManager.php
+++ src/ChatManager/Yugh/ChatManager.php
@@ -7,49 +7,54 @@ use pocketmine\utils\Config;
 
 use ChatManager\Yugh\listener\EventListener;
 use ChatManager\Yugh\utils\Messages;
+
 use ChatManager\Yugh\commands\PrivateMessageCommand;
+use ChatManager\Yugh\commands\JoinMessageCommand;
+use ChatManager\Yugh\commands\AdMessageCommand;
 
 class ChatManager extends PluginBase {
   
-  private const DISTANCE = 20;
-  
-  public $config;
+    private const DISTANCE = 20;
+    
+    public $config;
 
-  public $messages;
-  
-  public function onEnable() :void {
-    $this->config = new Config($this->getDataFolder(). "config.json", Config::JSON);
-    $this->loadConfig();
+    public $messages;
+    
+    public function onEnable() :void {
+        $this->config = new Config($this->getDataFolder(). "config.json", Config::JSON);
+        $this->loadConfig();
 
-    $this->messages = new Messages($this);
+        $this->messages = new Messages($this);
 
-    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 
-  }
-
-  public function onLoad() :void {
-    $this->registerCommands();
-  }
-  
-  private function loadConfig() :void {
-    if (!$this->config->exists("distance")) $this->config->set("distance", self::DISTANCE);
-
-    $this->config->save();
-  }
-
-  private function registerCommands() :void {
-    $map = $this->getServer()->getCommandMap();
-    $commands = [
-      new PrivateMessageCommand($this, "pm", "личное сообщение")
-    ];
-
-    foreach ($commands as $command) {
-      $map->register("ChatManager", $command);
     }
-  }
 
-  public function getFolder() :string {
-    return $this->getFile();
-  }
+    public function onLoad() :void {
+        $this->registerCommands();
+    }
+    
+    private function loadConfig() :void {
+        if (!$this->config->exists("distance")) $this->config->set("distance", self::DISTANCE);
+
+        $this->config->save();
+    }
+
+    private function registerCommands() :void {
+        $map = $this->getServer()->getCommandMap();
+        $commands = [
+            new PrivateMessageCommand($this, "pm", "личное сообщение"),
+            new JoinMessageCommand($this, "join", "сообщение при входе"),
+            new AdMessageCommand($this, "ad", "объявление")
+        ];
+
+        foreach ($commands as $command) {
+            $map->register("ChatManager", $command);
+        }
+    }
+
+    public function getFolder() :string {
+        return $this->getFile();
+    }
   
 }

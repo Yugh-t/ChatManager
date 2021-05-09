fca041a11eb6f48ef1a616841c976a84592079c1
Yugh-t <yugh-t.drake@mail.ru>
2021-05-09 17:17:12 +0300

    Update PrivateMessageCommand.php

--- src/ChatManager/Yugh/commands/PrivateMessageCommand.php
+++ src/ChatManager/Yugh/commands/PrivateMessageCommand.php
@@ -6,40 +6,39 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class PrivateMessageCommand extends Command {
+	
+	private $plugin;
 
-  private $plugin;
+	public function __construct($plugin, string $command, string $description) {
+		$this->plugin = $plugin;
 
-  public function __construct($plugin, string $command, string $description) {
-    $this->plugin = $plugin;
+		parent::__construct($command, $description);
+		$this->setPermission("private.message.command");
+	}
 
-    parent::__construct($command, $description);
-    $this->setPermission("private.message.command");
-  }
+	public function execute(CommandSender $sender, string $label, array $args) : bool {
+		$messages = $this->plugin->messages->getMessage("private-message-command");
 
-  public function execute(CommandSender $sender, string $label, array $args) :bool {
-    $messages = $this->plugin->messages->getMessage("private-message-command");
+		if (count($args) > 1) {
+			$player = $this->plugin->getServer()->getPlayer($args[0]);
+			$name = $args[0];
 
-  	if (count($args) > 1) {
-  	  $player = $this->plugin->getServer()->getPlayer($args[0]);
-  	  $name = $args[0];
+			array_shift($args);
 
-      array_shift($args);
+			if (!is_null($player)) {
+				if ($name != strtolower($sender->getName())) {
+					$message = implode(" ", $args);
+					$format = $messages["send-format"];
 
-  	  if (!is_null($player)) {
-  		if ($name != strtolower($sender->getName())) {
-  		  $message = implode(" ", $args);
-          $format = $messages["send-format"];
+					$format = str_replace("{sender}", $sender->getName(), $format);
+					$format = str_replace("{player}", $name, $format);
+					$format = str_replace("{message}", $message, $format);
 
-          $format = str_replace("{sender}", $sender->getName(), $format);
-          $format = str_replace("{player}", $name, $format);
-          $format = str_replace("{message}", $message, $format);
+					$player->sendMessage($format);
+				} else $sender->sendMessage($messages["message-not-to-me"]);
+			} else $sender->sendMessage($messages["player-not-found"]);
+		} else $sender->sendMessage($messages["not-confirm"]);
 
-  		  $player->sendMessage($format);
-  		} else $sender->sendMessage($messages["message-not-to-me"]);
-  	  } else $sender->sendMessage($messages["player-not-found"]);
-  	} else $sender->sendMessage($messages["not-confirm"]);
-
-    return true;
-  }
-
+		return true;
+	}
 }
